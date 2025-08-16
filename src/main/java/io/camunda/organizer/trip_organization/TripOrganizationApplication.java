8f3bf4d4c2d738a27b5b9a6bbed74e6138645ba1
martinakozul <tinakozul02@gmail.com>
2025-08-16 13:19:10 +0200

    autodeploy camunda files

--- src/main/java/io/camunda/organizer/trip_organization/TripOrganizationApplication.java
+++ src/main/java/io/camunda/organizer/trip_organization/TripOrganizationApplication.java
@@ -1,10 +1,15 @@
 package io.camunda.organizer.trip_organization;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TripOrganizationApplication {
+@Deployment(resources = {
+		"classpath*:/bpmn/**/*.bpmn",
+		"classpath*:/bpmn/**/*.dmn",
+		"classpath*:/bpmn/**/*.form"
+})public class TripOrganizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripOrganizationApplication.class, args);
